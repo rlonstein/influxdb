6149b95f3e55eb36b230c14e7ba6e636dbc2ccab
Edd Robinson <me@edd.io>
2017-01-12 17:54:15 +0000

    Fix subtle bugs and remove dead code

--- tsdb/engine/tsm1/engine_test.go
+++ tsdb/engine/tsm1/engine_test.go
@@ -822,6 +822,7 @@ func benchmarkEngine_WritePoints_Parallel(b *testing.B, batchSize int) {
 		b.StartTimer()
 
 		var wg sync.WaitGroup
+		errC := make(chan error, cpus)
 		for i := 0; i < cpus; i++ {
 			wg.Add(1)
 			go func(i int) {
@@ -829,11 +830,19 @@ func benchmarkEngine_WritePoints_Parallel(b *testing.B, batchSize int) {
 				from, to := i*batchSize, (i+1)*batchSize
 				err := e.WritePoints(pp[from:to])
 				if err != nil {
-					b.Fatal(err)
+					errC <- err
+					return
 				}
 			}(i)
 		}
 		wg.Wait()
+
+		close(errC)
+		for err := range errC {
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
 	}
 }
 
